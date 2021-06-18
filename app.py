cd0da98048abf8b758ea9c95f94ae5a5e95bcd8b
navneetkum <knavneet987@gmail.com>
2021-06-18 15:29:04 +0530

    change

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 from model import get_output
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-CORS(app)
-
+cors=CORS(app)
+app.config['CROSS-HEADERS']='Content-Type'
 @app.route("/")
+@cross_origin()
 def home():
     return "Hello Welcome to Waste Segregation!!"
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     path = request.get_json(force=True)
     url = path['image']
@@ -20,4 +22,4 @@ def predict():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run( app.run(debug=True, host="0.0.0.0",port=8080))
+    app.run()
